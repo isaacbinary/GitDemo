f7bbe649447ce602ebcd1d3129183f41895db4eb
HIMANSHU RAWAT <himanshurawat199995@gmail.com>
2022-03-16 13:20:52 +0530

    this is commit from imported repo

---
+++ src/test/java/qaclickacademy/MyClass.java
@@ -0,0 +1,14 @@
+package qaclickacademy;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Push code 1");
+		System.out.println("Push code 2");
+		System.out.println("Push code 3");
+
+	}
+
+}
